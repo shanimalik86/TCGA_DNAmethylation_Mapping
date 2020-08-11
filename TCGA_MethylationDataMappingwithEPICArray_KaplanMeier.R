4dfbc956fc69bfbc504876952fdf7db631ebe082
Zeeshan Fazal <47237921+shanimalik86@users.noreply.github.com>
2020-08-10 21:42:55 -0500

    Add files via upload

--- TCGA_MethylationDataMappingwithEPICArray_KaplanMeier.R
+++ TCGA_MethylationDataMappingwithEPICArray_KaplanMeier.R
@@ -1,17 +1,17 @@
-setwd("/Methylation_CorelationAnalysis_Selected/TCGA/")
+setwd("/Users/fazal2/Desktop/MethylationData_Updated/Methylation_CorelationAnalysis_Selected/TCGA/")
 library(tidyverse)
 require(data.table)
 library("survival")
 library("survminer")
 
-DMPs<-read_tsv("B3_Case1_Beta0.5_0.05FDR.txt")
-clinical<-read_tsv("tgct_tcga_clinical_data.tsv")
+DMPs<-read_tsv("/Users/fazal2/Desktop/tidy_practice/B3_Case1_Beta0.4_0.05FDR.txt")
+clinical<-read_tsv("/Users/fazal2/Desktop/tidy_practice/tgct_tcga_clinical_data.tsv")
 
 colnames(clinical)<-c("ID","PID","Sample_ID","CancerType","DSF_Time","DFS_Status")
-reform_clin=melt(clinical)
-colnames(reform_clin)<-c("ID","PID","Sample_ID","CancerType","DFS_Status","variable","Time")
+#reform_clin=melt(clinical)
+#colnames(reform_clin)<-c("ID","PID","Sample_ID","CancerType","DFS_Status","variable","Time")
 head(DMPs)
-head(reform_clin)
+#head(reform_clin)
 
 #create a list of the files from your target directory
 file_list <- list.files()
@@ -27,6 +27,7 @@ for (i in 1:length(file_list)){
   drop_na()
 }
 full=Reduce(merge,test)
+dim(full)
 
 #calculate mean of each sample
 Samples_Mean=full %>% 
@@ -39,11 +40,11 @@ reform_means=melt(Samples_Mean)
 colnames(reform_means)<-c("Sample_ID","Beta")
 
 #mapp clinical data
-Methy_Clinical=reform_clin %>% 
+Methy_Clinical=clinical %>% 
   filter(Sample_ID%in%reform_means$Sample_ID) %>% 
   full_join(reform_means,by="Sample_ID") %>% 
   drop_na() %>% 
-  select(c("Sample_ID","CancerType","DFS_Status","Time","Beta"))
+  select(c("Sample_ID","CancerType","DFS_Status","DSF_Time","Beta"))
 
 #spliting data by median Beta value
 Median_Beta=median(Methy_Clinical$Beta)
@@ -60,15 +61,13 @@ Methy_Clinical$event[event_occured]="1"
 Methy_Clinical$event[no_event]="0"
 
 
-Methy_Clinical$CancerType <- as.character(Methy_Clinical$CancerType)
-Methy_Clinical$CancerType[Methy_Clinical$CancerType == "Non-Seminomatous Germ Cell Tumor" | Methy_Clinical$CancerType == "Embryonal Carcinoma" ] <- "Non-Seminoma"
+#Methy_Clinical$CancerType <- as.character(Methy_Clinical$CancerType)
+Methy_Clinical$CancerType[Methy_Clinical$CancerType == "Non-Seminomatous Germ Cell Tumor" | Methy_Clinical$CancerType == "Non-Seminomatous Germ Cell Tumo" | Methy_Clinical$CancerType == "Embryonal Carcinoma" ] <- "Non-Seminoma"
 
-write_tsv(Methy_Clinical, path = "B3_Case1_Beta0.5_0.05FDR_Clinical.tsv")
+write_tsv(Methy_Clinical, path = "/Users/fazal2/Desktop/tidy_practice/B3_Case1_Beta0.4_0.05FDR_Methylation_Clinical.tsv")
 
 #run KM
-case <- read.table("B3_Case1_Beta0.5_0.05FDR_Clinical.tsv", quote="", comment="", sep="\t", header=TRUE, na.strings=c("NA","NaN", " ", "?"))
-head(case)
-fit <- survfit(Surv(case$Time, case$event) ~ case$level, data = case)
+fit <- survfit(Surv(as.numeric(Methy_Clinical$DSF_Time), as.numeric(Methy_Clinical$event)) ~ Methy_Clinical$level, data = Methy_Clinical)
 print(fit)
 summary(fit)
 
@@ -82,6 +81,6 @@ ggsurvplot(fit,
            palette = c("#E7B800", "#2E9FDF"))
 
 
-surv_diff <- survdiff(Surv(case$Time, case$event) ~ case$level, data = case)
+surv_diff <- survdiff(Surv(as.numeric(Methy_Clinical$DSF_Time), as.numeric(Methy_Clinical$event)) ~ Methy_Clinical$level, data = Methy_Clinical)
 surv_diff
 
